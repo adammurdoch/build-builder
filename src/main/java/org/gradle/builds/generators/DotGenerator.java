e5fc169d87c12d63b8848b9949b618c46cf83fc9
Adam Murdoch <adam@gradle.com>
2018-05-15 13:34:03 +1000

    Improve dependencies HTML report.

--- src/main/java/org/gradle/builds/generators/DotGenerator.java
+++ src/main/java/org/gradle/builds/generators/DotGenerator.java
@@ -8,54 +8,58 @@ import java.nio.file.Path;
 public class DotGenerator implements Generator<Model> {
     @Override
     public void generate(Model model, FileGenerator fileGenerator) throws IOException {
-        Path dotFile = model.getBuild().getRootDir().resolve("dependencies.dot");
-        fileGenerator.generate(dotFile, writer -> {
-            writer.println("digraph builds {");
-            writer.println("  graph [rankdir=\"LR\"]");
-            for (Build build : model.getBuilds()) {
-                writer.println("  subgraph cluster_build_" + build.getName() + " {");
-                writer.println("    color = \"blue\"");
-                for (Project project : build.getProjects()) {
-                    writer.println("    " + project.getName());
-                }
-                writer.println("  }");
-                for (Project project : build.getProjects()) {
-                    for (Dependency<Library<?>> library : project.getRequiredLibraries(Object.class)) {
-                        writer.print("  ");
-                        writer.print(project.getName());
-                        writer.print(" -> ");
-                        writer.print(library.getTarget().getDisplayName());
-                        writer.print(" [color = \"");
-                        writer.print(library.isApi() ? "black" : "grey56");
-                        writer.println("\"]");
-                    }
-                }
-            }
-            writer.println("}");
-        });
         Path htmlFile = model.getBuild().getRootDir().resolve("dependencies.html");
         fileGenerator.generate(htmlFile, writer -> {
             writer.println("<!DOCTYPE html>");
             writer.println("<html>");
             writer.println("<head>");
+            writer.println("<style>");
+            writer.println(".body { font-family: sans-serif; }");
+            writer.println(".mermaid {  }");
+            writer.println("</style>");
             writer.println("</head>");
             writer.println("<body>");
+            writer.println("<h2>Build dependencies</h2>");
             writer.println("<div class=\"mermaid\">");
             writer.println("graph LR");
             for (Build build : model.getBuilds()) {
+                writer.print("  ");
+                writer.println(build.getName());
+                for (Build dep : build.getDependsOn()) {
+                    writer.print("  ");
+                    writer.print(build.getName());
+                    writer.print(" --> ");
+                    writer.print(dep.getName());
+                    writer.println();
+                }
+            }
+            writer.println("</div>");
+            writer.println("<h2>Project dependencies</h2>");
+            writer.println("<div class=\"mermaid\">");
+            writer.println("graph LR");
+            for (Build build : model.getBuilds()) {
+                writer.println("  subgraph " + build.getName());
+                for (Project project : build.getProjects()) {
+                    writer.print("  ");
+                    writer.print(project.getName());
+                    writer.println();
+                }
+                writer.println("  end");
                 for (Project project : build.getProjects()) {
                     for (Dependency<Library<?>> library : project.getRequiredLibraries(Object.class)) {
                         writer.print("  ");
-                        writer.print(project.getName() + "(" + project.getName() + ")");
+                        writer.print(project.getName());
                         writer.print(library.isApi() ? " -- API --> " : " --> ");
-                        writer.print(library.getTarget().getDisplayName() + "(" + library.getTarget().getDisplayName() + ")");
+                        writer.print(library.getTarget().getDisplayName());
                         writer.println();
                     }
                 }
             }
             writer.println("</div>");
-            writer.println("<script src=\"https://unpkg.com/mermaid@7.1.2/dist/mermaid.min.js\"></script>");
-            writer.println("<script>mermaid.initialize({startOnLoad:true});</script>");
+            writer.println("<script src=\"https://unpkg.com/mermaid@8.0.0-rc.8/dist/mermaid.min.js\"></script>");
+            writer.println("<script>");
+            writer.println("mermaid.initialize({startOnLoad:true});");
+            writer.println("</script>");
             writer.println("</body>");
             writer.println("</html>");
         });
