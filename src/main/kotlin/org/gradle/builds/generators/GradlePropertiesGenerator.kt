9e378259610492f8048879961927325203369b72
Adam Murdoch <adam@gradle.com>
2019-09-13 11:57:33 +1000

    Tweak heap sizes.

--- src/main/kotlin/org/gradle/builds/generators/GradlePropertiesGenerator.kt
+++ src/main/kotlin/org/gradle/builds/generators/GradlePropertiesGenerator.kt
@@ -8,7 +8,7 @@ class GradlePropertiesGenerator : Generator<ConfiguredBuild> {
     override fun generate(build: ConfiguredBuild, fileGenerator: FileGenerator) {
         val readMe = build.rootDir.resolve("gradle.properties")
         val component = build.rootProject.component(HasHeapRequirements::class.java)
-        val min = component?.minHeapMegabytes ?: 64
+        val min = component?.minHeapMegabytes ?: 128
         val size = max(min, build.projects.size * 4)
         fileGenerator.generate(readMe) { writer ->
             writer.println("org.gradle.jvmargs=-Xmx${size}m")
